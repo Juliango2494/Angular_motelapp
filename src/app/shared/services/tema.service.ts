a572a35f0256fa81454654ee6bd3ef1588952b5b
Julian <juliango2494@gmail.com>
2025-09-22 09:29:17 -0500

    fix @for

--- src/app/shared/services/tema.service.ts
+++ src/app/shared/services/tema.service.ts
@@ -4,18 +4,17 @@ import { Injectable, signal } from '@angular/core';
   providedIn: 'root'
 })
 export class UiService {
-  // estado global del tema
+  
   private readonly _temaOscuro = signal(false);
 
-  // exposición de solo lectura
+ 
   readonly temaOscuro = this._temaOscuro.asReadonly();
 
-  // alternar tema
+  
   toggleTema() {
     this._temaOscuro.update(v => !v);
   }
 
-  // fijar tema explícitamente
   setTemaOscuro(valor: boolean) {
     this._temaOscuro.set(valor);
   }
